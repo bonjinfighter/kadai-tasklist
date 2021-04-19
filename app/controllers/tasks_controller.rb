7789fcff52311be0a626ce11f6b897bb2bb44708
yukihiro.namba <akauntodemo7@gmail.com>
2021-04-19 08:05:39 +0000

    saisaiteisyutsu

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,8 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:show, :edit, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
-      @task = current_user.tasks.build
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
